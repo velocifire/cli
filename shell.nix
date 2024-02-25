e91ab4aa0ce53e9f4b6b51eb2557e7efb307d6db
Dan Harper <dan.harper@homes.com.au>
2024-02-25 12:21:07 +1100

    Making a HTTP request

--- shell.nix
+++ shell.nix
@@ -7,10 +7,36 @@ mkShell {
     rustfmt
     rust-analyzer
     clippy
+    clang
+    llvmPackages.bintools
+    pkg-config
+    iconv
+    pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+    pkgs.darwin.apple_sdk.frameworks.CoreServices
+    pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
   ];
   shellHook = ''
   '';
 
   RUST_BACKTRACE = 1;
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+    # Add precompiled library to rustc search path
+    RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
+      # add libraries here (e.g. pkgs.libvmi)
+    ]);
+    # Add glibc, clang, glib and other headers to bindgen search path
+    BINDGEN_EXTRA_CLANG_ARGS = 
+    # Includes with normal include path
+    (builtins.map (a: ''-I"${a}/include"'') [
+      # add dev libraries here (e.g. pkgs.libvmi.dev)
+#      pkgs.glibc.dev 
+    ])
+    # Includes with special directory paths
+    ++ [
+      ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+      ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+      ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
+    ];
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
 use clap::Parser;
+use anyhow::{Context, Result};
+use reqwest;
+use tokio;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
     auth_key: String,
+
     #[arg(short = 'n', long)]
     service_name: String,
+
     #[arg(short, long)]
     session_id: String,
 }
-fn main() {
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let args = Args::parse();
-    
-    println!("Hello {}!", args.auth_key)
+    get_services(&args.auth_key).await?;
+    println!("Hello {}!", args.auth_key);
+    Ok(())
+}
+
+// call to the main server at app.velocifire.com to get the list of services
+async fn get_services(auth_key: &str) -> Result<()> {
+    let url = "https://app.velocifire.com/api/v1/services";
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .header("Authorization", format!("Bearer {}", auth_key))
+        .send()
+        .await
+        .context("Failed to send request")?;
+    let body = response.text().await.context("Failed to get response body")?;
+    println!("{}", body);
+    Ok(())
 }
